349a0433c89c73dbe97a2a21ad1586e55e67b811
Sebastian Tombe Campo (202110051) <johan.tombe@correounivalle.edu.co>
2023-06-15 12:30:59 -0500

    conf dockerfie para deploy-7

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/turbo_db',
+        default='postgresql://postgres:postgres@localhost/postgres',
         conn_max_age=600
     )
 }

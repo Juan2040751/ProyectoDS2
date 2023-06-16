54e53da1d1641e90e0a2cb73b01cf4419fbce07e
Sebastian Tombe Campo (202110051) <johan.tombe@correounivalle.edu.co>
2023-06-15 20:35:28 -0500

    deploy back-1

--- frontend/src/api/products.api.js
+++ frontend/src/api/products.api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const productApi = axios.create({
-    baseURL: "http://127.0.0.1:8000/products/",
+    baseURL: "https://tienda-service.onrender.com/products/",
 })
 
 export const getAllProducts = () => productApi.get("/");
